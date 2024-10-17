c0fb2151b487a5ff41c613909142839cf378235b
Sharkyiz <sa2006kevin@gmail.com>
2024-10-16 23:46:26 -0300

    a

---
+++ src/services/UserCreateService.js
@@ -0,0 +1,32 @@
+const { hash } = require("bcryptjs");
+
+const AppError = require("../utils/AppError");
+
+class UserCreateService {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async execute({ name, email, password }) {
+    const checkUserExists = await this.userRepository.findByEmail(
+      email
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const userCreated = await this.userRepository.create({
+    
+      email,  
+      name,
+      password: hashedPassword
+    });
+
+    return userCreated;
+  }
+}
+
+module.exports = UserCreateService;
